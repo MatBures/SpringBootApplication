5feed48794237660619d89997a301691ec99fd97
Matyáš Bureš <117597975+MatBures@users.noreply.github.com>
2023-07-13 16:49:22 +0200

    (1PF-153) Add TestContainer for PostgreSQL

--- src/test/java/org/firstPF/ApplicationTests.java
+++ src/test/java/org/firstPF/ApplicationTests.java
@@ -2,10 +2,30 @@ package org.firstPF;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @SpringBootTest
+@Testcontainers
 class ApplicationTests {
+
+	@Container
+	public static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:latest")
+			.withDatabaseName("postgres")
+			.withUsername("postgres")
+			.withPassword("admin");
+
+
+	@DynamicPropertySource
+	static void configurePostgres(DynamicPropertyRegistry registry) {
+		postgres.start();
+		registry.add("spring.datasource.url", () -> String.format("jdbc:postgresql://localhost:%d/postgres", postgres.getFirstMappedPort()));
+		registry.add("spring.datasource.username", () -> "postgres");
+		registry.add("spring.datasource.password", () -> "admin");
+	}
 
 	@Test
 	void contextLoads() {
