7da6ee556df3e6e044ec5054830077d7c74654f0
Matyáš Bureš <117597975+MatBures@users.noreply.github.com>
2023-07-13 15:39:51 +0200

    (1PF-153) Add Docker to TestContainerConfig

--- src/test/java/org/firstPF/ApplicationTests.java
+++ src/test/java/org/firstPF/ApplicationTests.java
@@ -2,8 +2,10 @@ package org.firstPF;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = TestContainerConfig.class)
 class ApplicationTests {
 
 	@Test
