51fbf42b252800a76f2acb17916b6e9c43ba299c
Matyáš Bureš <117597975+MatBures@users.noreply.github.com>
2023-07-24 12:06:05 +0200

    (1PF-153) Add services and controllers

--- src/main/java/org/firstPF/controllers/EmployeeController.java
+++ src/main/java/org/firstPF/controllers/EmployeeController.java
@@ -1,10 +1,17 @@
 package org.firstPF.controllers;
 
+import org.firstPF.entities.Employee;
 import org.firstPF.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/employees")
 public class EmployeeController {
     private final EmployeeService employeeService;
 
@@ -12,4 +19,66 @@ public class EmployeeController {
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
     }
+
+    @PostMapping
+    public ResponseEntity<?> createEmployee(@RequestBody Employee employee) {
+        // Check if providerId is provided
+        if (employee.getProvider() == null || employee.getProvider().getId() == null) {
+            return ResponseEntity.badRequest().body("ProviderId is required.");
+        }
+
+        Employee createdEmployee = employeeService.createEmployee(employee);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdEmployee);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        List<Employee> employees = employeeService.getAllEmployees();
+        return new ResponseEntity<>(employees, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+        Employee employee = employeeService.getEmployeeById(id).orElse(null);
+        if (employee == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(employee, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateEmployee(@PathVariable Long id, @RequestBody Employee updatedEmployee) {
+        Employee employee = employeeService.getEmployeeById(id).orElse(null);
+        if (employee == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        // Check if providerId and customerId are provided in the updatedOffer
+        if (updatedEmployee.getProvider() == null || updatedEmployee.getProvider().getId() == null) {
+            return ResponseEntity.badRequest().body("ProviderId is required for updating an employee.");
+        }
+
+        // Update the employee properties with the updatedEmployee
+        employee.setFirstName(updatedEmployee.getFirstName());
+        employee.setLastName(updatedEmployee.getLastName());
+        employee.setEmail(updatedEmployee.getEmail());
+        employee.setDateOfBirth(updatedEmployee.getDateOfBirth());
+        employee.setContactNumber(updatedEmployee.getContactNumber());
+        employee.setProvider(updatedEmployee.getProvider());
+
+        Employee updatedEmployeeEntity = employeeService.createEmployee(employee);
+        return new ResponseEntity<>(updatedEmployeeEntity, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteEmployee(@PathVariable Long id) {
+        Optional<Employee> employee = employeeService.getEmployeeById(id);
+        if (employee.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        employeeService.deleteEmployee(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- src/main/java/org/firstPF/controllers/ProviderController.java
+++ src/main/java/org/firstPF/controllers/ProviderController.java
@@ -1,16 +1,71 @@
 package org.firstPF.controllers;
 
 
+import org.firstPF.entities.Provider;
 import org.firstPF.services.ProviderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/providers")
 public class ProviderController {
     private final ProviderService providerService;
 
     @Autowired
     public ProviderController(ProviderService providerService) {
         this.providerService = providerService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Provider> createProvider(@RequestBody Provider provider) {
+        Provider createdProvider = providerService.createProvider(provider);
+        return new ResponseEntity<>(createdProvider, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Provider>> getAllProviders() {
+        List<Provider> providers = providerService.getAllProviders();
+        return new ResponseEntity<>(providers, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Provider> getProviderById(@PathVariable Long id) {
+        Provider provider = providerService.getProviderById(id).orElse(null);
+        if (provider == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(provider, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Provider> updateProvider(@PathVariable Long id, @RequestBody Provider updatedProvider) {
+        Provider provider = providerService.getProviderById(id).orElse(null);
+        if (provider == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        // Update the provider properties with the updatedProvider
+        provider.setName(updatedProvider.getName());
+        provider.setAddress(updatedProvider.getAddress());
+        provider.setEmail(updatedProvider.getEmail());
+        provider.setContactNumber(updatedProvider.getContactNumber());
+
+        Provider updatedProviderEntity = providerService.createProvider(provider);
+        return new ResponseEntity<>(updatedProviderEntity, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProvider(@PathVariable Long id) {
+        Optional<Provider> provider = providerService.getProviderById(id);
+        if (provider.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        providerService.deleteProvider(id);
+        return ResponseEntity.noContent().build();
     }
 }
